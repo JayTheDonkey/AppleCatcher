10e87530ddf466fff23274a0c6fc4beb70d3c884
cescott <chuckescott@comcast.net>
2016-09-15 10:00:07 -0700

    Starting apple class
    also get coords

---
+++ html/src/main/java/playn/html/Apple.java
@@ -0,0 +1,22 @@
+package playn.html;
+
+import playn.core.Image;
+
+/**
+ * Created by charl on 9/15/2016.
+ */
+public class Apple {
+
+    protected Image appleImg;
+    protected int[] coords = new int[2];
+
+
+    public Apple() {
+
+    }
+
+    public int[] getCoords() {
+        return coords;
+    }
+    
+}
